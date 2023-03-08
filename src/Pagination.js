12bc7e1929ec6c02c4bd658675430c43b69c1771
Abhinav Krishna <123726072+AbhinvKrishna@users.noreply.github.com>
2023-03-08 15:37:08 -0800

    ui

--- src/Pagination.js
+++ src/Pagination.js
@@ -7,11 +7,13 @@ function Pagination() {
   const {nextPage,prevPage , nbPages, page} = useGlobalContext();
 
   return (
-    <div style={{width:"400px", display:"flex", justifyContent:"space-between"}}>
-      <p>{page}|{nbPages}</p>
-      <button  style={{background:"black", color:"white", cursor:"pointer"}} onClick={()=> prevPage()}>&larr; Previous</button>
-      <button  style={{background:"black", color:"white", cursor:"pointer"}}onClick={()=> nextPage()} >Next &rarr;</button>
+    <>
+    <div style={{width:"500px", display:"flex", justifyContent:"space-between",marginLeft:"500px", marginTop:"30px"}}>
+      <button disabled={page===0} style={{background:"black", color:"white", cursor:"pointer", height:"30px",border: "3px solid orange",borderRadius:"10px",}} onClick={()=> prevPage()}>&larr; Previous</button>
+      <button disabled={page===nbPages-1}  style={{background:"black", color:"white", cursor:"pointer",height:"30px",border: "3px solid orange",borderRadius:"10px"}}onClick={()=> nextPage()} >Next &rarr;</button>
     </div>
+     <p className='pagedesc'>{page}|{nbPages}</p>
+     </>
   )
 }
 
